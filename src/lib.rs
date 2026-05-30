ab07f7ef329ddc65cdb5935bc6e23f4ca9ab612d
Omochi <251449262+omochikaeri15@users.noreply.github.com>
2026-05-30 16:46:37 -0400

    settings fix and pack feature gate

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
-pub mod pack;
+#[cfg(feature = "pack")] pub mod pack;
+// TODO: #[cfg(feature = "unite")] pub mod unite;
+// TODO: #[cfg(feature = "bcu")] pub mod bcu;
 pub mod animation;
 pub mod cat;
+// TODO: pub mod enemy;
 pub mod common;
+
+pub use image;
+pub use serde;
