fa15a051c3d827623e2ba4256d36fd6282b7298e
Omochi <251449262+omochikaeri15@users.noreply.github.com>
2026-06-02 21:48:15 -0400

    tsv garbage checking

--- src/pack/utils/verify.rs
+++ src/pack/utils/verify.rs
@@ -13,7 +13,7 @@ pub fn check_integrity(data: &[u8], filename: &str) -> bool {
         return data.len() >= 4 && data.starts_with(&[0x89, 0x50, 0x4E, 0x47]);
     }
 
-    if lower_name.ends_with(".csv") || lower_name.ends_with(".list") || lower_name.ends_with(".json")
+    if lower_name.ends_with(".csv") || lower_name.ends_with(".tsv") || lower_name.ends_with(".list") || lower_name.ends_with(".json")
         || lower_name.ends_with(".maanim") || lower_name.ends_with(".mamodel") || lower_name.ends_with(".imgcut")
     {
         return std::str::from_utf8(data).is_ok();
