ed3efb8bd9ff27f06e60c173591e2652b599426e
Omochi <251449262+omochikaeri15@users.noreply.github.com>
2026-06-14 06:33:58 -0400

    set-up

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 // TODO: #[cfg(feature = "apk")] pub mod apk; [file "Bouncer" and apk structure]
 #[cfg(feature = "pack")] pub mod pack;
-// TODO: #[cfg(feature = "event")] pub mod event; [event data stuff: https://codeberg.org/fieryhenry/bcdd]
+// TODO: #[cfg(feature = "event")] pub mod event; [event data stuff like bcdd does]
 // TODO: #[cfg(feature = "unite")] pub mod unite; [decrypt "arc" nintendo files and handle formats such as btrx]
 // TODO: #[cfg(feature = "bcu")] pub mod bcu; [decrypt bcuzip & bcuzips, convert bcu formats to game formats]
 #[cfg(feature = "graphics")] pub mod graphics;
